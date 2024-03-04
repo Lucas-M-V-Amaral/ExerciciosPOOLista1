9d3e6a6bf62fe4aa800f221470a6c998556abeab
Lucas Amaral <125374054+Lucas-M-V-Amaral@users.noreply.github.com>
2024-03-03 22:47:31 -0300

    Pessoa feita
    Ainda vou ver direito

---
+++ ExerciciosPOOLista1/ContaBancaria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosPOOLista1
+{
+    public class ContaBancaria
+    {
+        /*
+        2. Implemente uma classe chamada “ContaBancária” que possua atributos para armazenar o
+        número da conta, nome do titular e saldo. Adicione métodos para realizar depósitos e
+        saques.
+        */
+
+        public int NumeroDaConta { get; set; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; set; }
+
+        public ContaBancaria (int numeroDaConta, string nomeTitular, double saldo)
+        {
+            NumeroDaConta = numeroDaConta;
+            NomeTitular = nomeTitular;
+            Saldo = saldo;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            Saldo -= valor;
+        }
+    }
+}
---
+++ ExerciciosPOOLista1/Pessoa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosPOOLista1
+{
+    public class Pessoa
+    {
+        /*                      EXERCICIOS POO – PARTE 2
+        1. Crie uma classe chamada “Pessoa” que possua atributos para armazenar nome, idade e
+        profissão. Implemente métodos para calcular a idade em anos bissextos e exibir
+        informações da pessoa. 
+        */
+
+        public string Nome { get; set; }
+        public int Idade { get; set;}
+        public string Profissao { get; set; }
+
+        public Pessoa(string nome, int idade, string profissao)
+        {
+            Nome = nome;
+            Idade = idade;
+            Profissao = profissao;
+        }
+
+        public void CalculaIdadeBissextos()
+        {
+            int anoAtual = int.Parse(DateTime.Now.ToString("yyyy"));
+            int anoDeNascimento = anoAtual - Idade;
+            int quantBissextos = 0;
+
+            for (int i = anoDeNascimento; i <= anoAtual; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    if (i % 100 != 0)
+                    {
+                        quantBissextos++;
+                    }
+                    else if (i % 100 == 0 && i % 400 == 0)
+                    {
+                        quantBissextos++;
+                    }
+                }
+            }
+            Console.WriteLine($"Durante sua vida você passou por {quantBissextos} anos bissextos.");
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"{Nome} tem {Idade} anos e trabalha como {Profissao}.");
+        }
+    }
+}
